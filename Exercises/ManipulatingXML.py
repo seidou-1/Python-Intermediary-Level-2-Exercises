7e1b0a863805547a9c549d3e8920a44a03bf103c
MB <mbarry323@gmail.com>
2018-07-24 23:38:27 -0400

    created sample xml file and printed out properties

--- Exercises/ManipulatingXML.py
+++ Exercises/ManipulatingXML.py
@@ -5,8 +5,16 @@
 """
 Operating on the DOM
 """
-
-# def main():
+import xml.dom.minidom
+def main():
+    
+    #Using the parse() function to load and parse the sample data XML file
+    doc = xml.dom.minidom.parse("samplexml.xml")
+    
+    #Print out the document node and the name of the first child tag
+    #nodeName, firstChild, and tagName are all standard properties of DOM
+    print(doc.nodeName) 
+    print(doc.firstChild.tagName)
     
     
 
@@ -16,6 +24,6 @@ Operating on the DOM
 
 
 
-# 
-# if __name__ == "__main__":
-#     main();
+ 
+if __name__ == "__main__":
+    main();
