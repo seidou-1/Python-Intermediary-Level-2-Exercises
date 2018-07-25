a587db8703b30e6a9b8793215fdc3e514499fd1b
MB <mbarry323@gmail.com>
2018-07-24 23:50:12 -0400

    getElementsByTagName

--- Exercises/ManipulatingXML.py
+++ Exercises/ManipulatingXML.py
@@ -6,17 +6,23 @@
 Operating on the DOM
 """
 import xml.dom.minidom
+
 def main():
     
     #Using the parse() function to load and parse the sample data XML file
-    doc = xml.dom.minidom.parse("samplexml.xml")
+    doc = xml.dom.minidom.parse("/Users/laptop/Development/GitHub/Python/Intermediary Level 2 Examples/samplexml.xml")
     
     #Print out the document node and the name of the first child tag
     #nodeName, firstChild, and tagName are all standard properties of DOM
     print(doc.nodeName) 
     print(doc.firstChild.tagName)
     
+    #Get list of XML tags from the document and print each one
+    skills = doc.getElementsByTagName("skill") #The tag i'm looking for is the skill tag
+    print("%d skills: " % skills.length)
     
+    for skill in skills:
+        print(skill.getAttribute("name")) #This prints out each individual skill
 
 
 
